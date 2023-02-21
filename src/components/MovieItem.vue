d87ace50694ff257db61d986666a885d5cbbe33b
Азрет Елеев <azreteleev@MacBook-Air-Azret.local>
2023-02-21 22:03:28 +0300

    add ratings

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -7,12 +7,14 @@
     />
     <div class="movie-item__wrap">
       <div class="movie-item__info">
-        <h5 class="movie-item__title">{{ movie.name }}</h5>
+        <h5 class="movie-item__title">
+          {{ movie.name || movie.alternativeName }}
+        </h5>
         <span class="movie-item__year">{{ movie.year }}</span>
       </div>
       <div class="movie-item__btns row">
         <div class="col">
-          <div type="button" class="btn btn-outline-danger">
+          <div type="button" class="btn btn-outline-danger movie-item__like">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -28,22 +30,45 @@
             </svg>
           </div>
         </div>
-        <div class="col movie-item__r-wrap">
-          <div class="btn">
+        <div class="movie-item__rating-wrap col align-items-center">
+          <div
+            class="col movie-item__rating-item movie-item__rating-item_imdb"
+            v-if="isImdbRating"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="#FFD700"
-              class="bi bi-star-fill"
+              class="bi bi-star-fill movie-item__star"
               viewBox="0 0 16 16"
             >
               <path
                 d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
               />
             </svg>
+            <span class="movie-item__rating">{{ movie.rating.imdb }}</span>
+            <span class="movie-item__resource">IMDB</span>
           </div>
-          <span class="movie-item__rating">{{ movie.rating.imdb }}</span>
+          <div
+            class="col movie-item__rating-item movie-item__rating-item_kp"
+            v-if="isKpRating"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#FFD700"
+              class="bi bi-star-fill movie-item__star"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+              />
+            </svg>
+            <span class="movie-item__rating">{{ kpRating }}</span>
+            <span class="movie-item__resource">Кинопоиск</span>
+          </div>
         </div>
       </div>
     </div>
@@ -62,6 +87,15 @@ export default {
   computed: {
     posterBg() {
       return this.movie.poster.url;
+    },
+    kpRating() {
+      return Number(this.movie.rating.kp).toFixed(1);
+    },
+    isImdbRating() {
+      return Boolean(this.movie.rating.imdb);
+    },
+    isKpRating() {
+      return Boolean(this.movie.rating.kp);
     },
   },
 };
